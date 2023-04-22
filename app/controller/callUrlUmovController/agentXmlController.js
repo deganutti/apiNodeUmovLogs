a31a1d5f9894758d54069637ed848d5fd377fb0f
Luiz Gabriel Deganutti <deganutti@outlook.com.br>
2023-04-22 11:52:07 -0300

    Atualização de função, assim fica mais limpa e facil de entender.

--- app/controller/callUrlUmovController/agentXmlController.js
+++ app/controller/callUrlUmovController/agentXmlController.js
@@ -9,11 +9,11 @@ module.exports = {
 
     const { apikey } = req.params;
 
-/*
-&initialInsertDateTime=2023-02-03 00:00:00
- */
+                /*
+                &initialInsertDateTime=2023-02-03 00:00:00
+                */
 
-    for (page = 1; page < pages; page++) {
+    for (page = 0; page < pages; page++) {
       var options = {
         method: "GET",
         url: `https://api.umov.me/CenterWeb/api/${apikey}/agent.xml?paging.page=${page}`,
@@ -26,85 +26,78 @@ module.exports = {
         url: `http://localhost:88/ambienteApiKey/${apikey}`,
       };
 
-      await axios
-        .request(options)
-        .then(function (response) {
-          let attributos = convert.xml2js(response.data, {
-            compact: false,
-            spaces: 4,
-          });
+      await axios.request(options).then(function (response) {
+        let attributos = convert.xml2js(response.data, {
+          compact: true,
+          spaces: 4,
+        });
+        let totalPorPagina = attributos["result"]['size']._text;
+        let detalhes = attributos["result"]["entries"]["entry"]
 
-          const totalAgentPage =
-            attributos.elements[0].elements[1].elements[0].text;
+        if(detalhes){
+          let agent = detalhes[0]['_attributes'];
 
-          console.log("total ", totalAgentPage);
-
-          if (totalAgentPage !== "0") {
-            /*
-                    console.log("Retorno 1",attributos.elements[0]); 
-                    console.log("Retorno 2",attributos.elements[0].elements[1].elements[0]); 
-                    console.log("Retorno 3",attributos.elements[0].elements[1].elements[0].text); 
-*/
-
-            //  var id = attributos.elements[0].elements[2].elements[1].attributes.id;
-            //   var link = attributos.elements[0].elements[2].elements[1].attributes.link;
-
-            var registros = attributos.elements[0].elements[2].elements.length;
-
-            // console.log("Total de registros: ", registros);
-            // console.log("reg", attributos.elements[0].elements[2].elements.length);
-
-            for (i = 0; i < registros; i++) {
-              const id =
-                attributos.elements[0].elements[2].elements[i].attributes.id;
-              const link =
-                attributos.elements[0].elements[2].elements[i].attributes.link;
-              const link2 = link.replace("/agent/", "");
-
-              //       console.log("id", id);
-              //      console.log("link", link);
-
-              axios.request(ambienteOptions).then((resAmbiente) => {
-                const id_ambiente = resAmbiente.data.id;
-                //       console.log( "ID Ambiente:",id_ambiente,"\n","ID: ",id,"\n","Link: ",link);
-                /**
-                 * Retorna na variavel, id_ambiente
-                 *  SELECT `id`, `descricao`, `apikey`, `createdAt`, `updatedAt` FROM `Ambiente` AS `Ambiente` WHERE `Ambiente`.`apikey` = '36376e975e5cf31d52f1590e9600ffeb5dfa1f' LIMIT 1
-                 */
-
-                /**
-                 * /ambienteApiKey/:apikey
-                 */
-
+          console.log(`Quantidade página: ${totalPorPagina}`);
+          console.log(`Detalhes: ${detalhes}`);
+          for (let i = 0; i < totalPorPagina; i++) {
+            let id = detalhes[i]['_attributes'].id;
+            let link = detalhes[i]['_attributes'].link;
+            link = link.replace("/agent/", "")
+            console.log(`id: ${id}`);
+            console.log(`link: ${link}`);
+            console.log(`Total -> ${i}`);
+  
+            axios.request(ambienteOptions).then(
+              (resAmbiente) => {
+                
+              const id_ambiente = resAmbiente.data.id;
+              //       console.log( "ID Ambiente:",id_ambiente,"\n","ID: ",id,"\n","Link: ",link);
+              /**
+               * Retorna na variavel, id_ambiente
+               *  SELECT `id`, `descricao`, `apikey`, `createdAt`, `updatedAt` FROM `Ambiente` AS `Ambiente` WHERE `Ambiente`.`apikey` = '36376e975e5cf31d52f1590e9600ffeb5dfa1f' LIMIT 1
+               */
+  
+              /**
+               * /ambienteApiKey/:apikey
+               */
+              if (totalPorPagina !== "0") {
                 var agentBDOptions = {
                   method: "POST",
-                  url: `http://localhost:88/agent/${id_ambiente}/${id}/${link2}`,
+                  url: `http://localhost:88/agent/${id_ambiente}/${id}/${link}`,
                 };
                 //   console.log("ID Ambiente : ", id_ambiente);
                 //   console.log("ID Agent : ", id);
                 //   console.log("Link : ", link2);
-                axios
-                  .request(agentBDOptions)
-                  .then((resAgentBd) => {
-                    //  console.log(resAgentBd.data);
+                axios.request(agentBDOptions).then((resAgentBd) => {
+                     console.log(resAgentBd.data);
                   })
                   .catch((e) => {
                     console.error(e);
                   });
-              });
-            }
-          } else {
-            page = pages; 
-            return res.json({
-                status: 200,
-                Message: "Consulta finalizada",
-              });
-          
-          }         
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-    }
-  },
+              console.log(`Detalhes dos dados ${detalhes.length}`);
+
+              } else {
+                page = pages; 
+                return res.json({
+                    status: 200,
+                    Message: "Consulta finalizada",
+                  });
+              
+              }       
+          //    return res.json(detalhes);
+            }).catch(function (error) {
+                console.error(error);
+              }
+            );
+  
+          }
+        } else {
+          return res.json({
+            status: 200,
+            Message: "Consulta finalizada",
+          });
+        }
+      });
+    } 
+  }
 };
