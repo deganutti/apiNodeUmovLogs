7a47ecee38311ba3ee9e99b23ef384b1c583849a
Luiz Gabriel Deganutti <deganutti@outlook.com.br>
2023-04-22 12:14:00 -0300

    Ajustes, limpeza de lixo

--- app/controller/callUrlUmovController/agentXmlController.js
+++ app/controller/callUrlUmovController/agentXmlController.js
@@ -8,11 +8,7 @@ module.exports = {
     const pages = 999999999999999999999999;
 
     const { apikey } = req.params;
-
-                /*
-                &initialInsertDateTime=2023-02-03 00:00:00
-                */
-
+ 
     for (page = 0; page < pages; page++) {
       var options = {
         method: "GET",
@@ -31,65 +27,63 @@ module.exports = {
           compact: true,
           spaces: 4,
         });
-        let totalPorPagina = attributos["result"]['size']._text;
-        let detalhes = attributos["result"]["entries"]["entry"]
+        let totalPorPagina = attributos["result"]["size"]._text;
+        let detalhes = attributos["result"]["entries"]["entry"];
 
-        if(detalhes){
-          let agent = detalhes[0]['_attributes'];
+        if (detalhes) {
+          let agent = detalhes[0]["_attributes"];
 
           console.log(`Quantidade página: ${totalPorPagina}`);
           console.log(`Detalhes: ${detalhes}`);
           for (let i = 0; i < totalPorPagina; i++) {
-            let id = detalhes[i]['_attributes'].id;
-            let link = detalhes[i]['_attributes'].link;
-            link = link.replace("/agent/", "")
+            let id = detalhes[i]["_attributes"].id;
+            let link = detalhes[i]["_attributes"].link;
+            link = link.replace("/agent/", "");
             console.log(`id: ${id}`);
             console.log(`link: ${link}`);
             console.log(`Total -> ${i}`);
-  
-            axios.request(ambienteOptions).then(
-              (resAmbiente) => {
-                
-              const id_ambiente = resAmbiente.data.id;
-              //       console.log( "ID Ambiente:",id_ambiente,"\n","ID: ",id,"\n","Link: ",link);
-              /**
-               * Retorna na variavel, id_ambiente
-               *  SELECT `id`, `descricao`, `apikey`, `createdAt`, `updatedAt` FROM `Ambiente` AS `Ambiente` WHERE `Ambiente`.`apikey` = '36376e975e5cf31d52f1590e9600ffeb5dfa1f' LIMIT 1
-               */
-  
-              /**
-               * /ambienteApiKey/:apikey
-               */
-              if (totalPorPagina !== "0") {
-                var agentBDOptions = {
-                  method: "POST",
-                  url: `http://localhost:88/agent/${id_ambiente}/${id}/${link}`,
-                };
-                //   console.log("ID Ambiente : ", id_ambiente);
-                //   console.log("ID Agent : ", id);
-                //   console.log("Link : ", link2);
-                axios.request(agentBDOptions).then((resAgentBd) => {
-                     console.log(resAgentBd.data);
-                  })
-                  .catch((e) => {
-                    console.error(e);
-                  });
-              console.log(`Detalhes dos dados ${detalhes.length}`);
 
-              } else {
-                page = pages; 
-                return res.json({
+            axios
+              .request(ambienteOptions)
+              .then((resAmbiente) => {
+                const id_ambiente = resAmbiente.data.id;
+                //       console.log( "ID Ambiente:",id_ambiente,"\n","ID: ",id,"\n","Link: ",link);
+                /**
+                 * Retorna na variavel, id_ambiente
+                 *  SELECT `id`, `descricao`, `apikey`, `createdAt`, `updatedAt` FROM `Ambiente` AS `Ambiente` WHERE `Ambiente`.`apikey` = '36376e975e5cf31d52f1590e9600ffeb5dfa1f' LIMIT 1
+                 */
+
+                /**
+                 * /ambienteApiKey/:apikey
+                 */
+                if (totalPorPagina !== "0") {
+                  var agentBDOptions = {
+                    method: "POST",
+                    url: `http://localhost:88/agent/${id_ambiente}/${id}/${link}`,
+                  };
+                  //   console.log("ID Ambiente : ", id_ambiente);
+                  //   console.log("ID Agent : ", id);
+                  //   console.log("Link : ", link2);
+                  axios
+                    .request(agentBDOptions)
+                    .then((resAgentBd) => {
+                      console.log(resAgentBd.data);
+                    })
+                    .catch((e) => {
+                      console.error(e);
+                    });
+                } else {
+                  page = pages;
+                  return res.json({
                     status: 200,
                     Message: "Consulta finalizada",
                   });
-              
-              }       
-          //    return res.json(detalhes);
-            }).catch(function (error) {
+                }
+                //    return res.json(detalhes);
+              })
+              .catch(function (error) {
                 console.error(error);
-              }
-            );
-  
+              });
           }
         } else {
           return res.json({
@@ -98,6 +92,6 @@ module.exports = {
           });
         }
       });
-    } 
-  }
+    }
+  },
 };
