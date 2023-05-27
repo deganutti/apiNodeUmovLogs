ab2de71d0a4d1c9c27f02061dc43e02696d6c4e0
Luiz Gabriel Deganutti <deganutti@outlook.com.br>
2023-05-27 09:45:42 -0300

    Ajuste de função.

--- app/Routes/routesUmov.js
+++ app/Routes/routesUmov.js
@@ -28,14 +28,19 @@ routesUmov.get("/agent/:apikey",agentXmlController.getAgentXml);
 
 
 routesUmov.get("/agent",umovAgentController.index);
+
 routesUmov.post("/agent/:id_ambiente/:id_agente/:link_agente",umovAgentController.store);
 
 // routesUmov.get("/agentdetail/",umovAgentDetailController.index);
-routesUmov.post("/agentdetail/:id_ambiente/:id_agente",umovAgentDetailController.store);
+//routesUmov.post("/agentdetail/:id_ambiente/:id_agente",umovAgentDetailController.store);
+routesUmov.post("/agentdetail",umovAgentDetailController.store);
 
 routesUmov.get("/agentUmov",umovAgentDetailController.getAgentDetail);
-routesUmov.get("/newAgentDetail/:apiKey/:agent",umovAgentDetailController.getAgentDetail);
 
+routesUmov.get("/newAgentDetail/:apiKey/:agent",umovAgentDetailController.getAgentDetail); 
+
+
+routesUmov.post("/newAgentDetail",umovAgentDetailController.store);
 
 routesUmov.post("/newAgentDetail2",umovAgentDetailController.store);
 
